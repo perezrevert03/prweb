6eff0ec4ae5b3c860ce66ddbe0fb437b551f52ca
Carles Pérez Revert <carlesperezmxt37@gmail.com>
2019-02-27 20:35:30 +0100

    Remove unusued components

--- src/app/components/bootstrap/media-objects/media-objects.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-media-objects',
-    templateUrl: './media-objects.component.html',
-    styleUrls: ['./media-objects.component.scss']
-})
-
-export class MediaObjectsComponent { }
